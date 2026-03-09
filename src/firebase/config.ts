9541fa5eec0f11d0e7c6e1650f0c038059dfbab5
Pirates Cloths <piratescloths@gmail.com>
2026-03-09 11:33:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8721474096-a16f9",
+  "appId": "1:971929184244:web:6944d0e1201a37aa787ba1",
+  "apiKey": "AIzaSyDOW9mkmjQZxSGywf170-1a47ClRev8YZM",
+  "authDomain": "studio-8721474096-a16f9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "971929184244"
+};
